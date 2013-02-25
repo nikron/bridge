748cf04a0f7ffbb94502bcad2a8fcb6bdd303008
Nikhil Bysani <nbysani2@illinois.edu>
2013-02-25 17:07:46 -0600

    started working on the state tracking for each asset

--- bridge/services/model/model.py
+++ bridge/services/model/model.py
@@ -1,52 +1,20 @@
-from abc import ABCMeta, abstractmethod
 import uuid
-
-class States():
-    def __init__(self, current, states):
-        self.states = {}
-        i = 1
-
-        for state in states:
-            self.states[state] = i
-            i = i * 2
-
-        self.current = current # assume the first state is the first current
-        #state
-
-    def transition(self, state):
-        #mildy sure taking a 2^n - 2^k where n and k are nonzero are unique
-        edge = self.find_edge(self.current,state)
-        self.current = state
-
-        return edge
-
-    def find_edge(self, state1, state2):
-        edge = self.states[state1] - self.states[state2]
-        return edge
-
-class Asset(metaclass=ABCMeta):
-    def __init__(self, real_id, current, states, events):
-        self.states = States(current, states)
-
-        events.orient(states)
-        self.events =  events
-
+class Asset():
+    def __init__(self, real_id, current, states, triggers):
+        self.states = States(current, states, triggers)
         self.uuid = uuid.uuid1()
         self.real_id = real_id
 
-
     def state_transition(state):
-        edge = self.states.transition(state)
-        self.events.trigger(edge)
-
+        self.states.transition(state)
 
 class OnOffAsset(Asset):
     state_names = ['unknown', 'on', 'off', 'pending on', 'pending off']
-    def __init__(self, real_id, events, current='unknown'):
+    def __init__(self, real_id, events, current='unknown', None):
         super().__init__(real_id, current, self.state_names, events)
 
     def turn_off(self):
-        self.state_transition('off')
+        self.state_transition('pending off')
 
     def turn_on(self):
-        self.state_transition('off')
+        self.state_transition('pending on')
---
+++ bridge/services/model/states.py
@@ -0,0 +1,71 @@
+import uuid
+
+class Trigger():
+    def __init__(self, state1, state2, func):
+        self.key = uuid.uuid1()
+        self.state1 = state1
+        self.state2 = state2
+        self.func = func
+
+    def __eq__(self, other):
+        if self.key == other.key:
+            return True
+        else:
+            return False
+
+    def trigger():
+        self.func()
+
+class States():
+    def __init__(self, current, states, triggers):
+        self.states = {}
+        i = 1
+
+        for state in states:
+            self.states[state] = i
+            i = i * 2
+
+        self.current = current
+
+        self.triggers = triggers
+        self.trigger_mesh = {}
+        self.orient()
+
+    def orient_trigger(self, trigger):
+        edge = self.find_edge(trigger.state1, trigger.state2)
+
+        if edge in self.triggers_mesh:
+            self.events_mesh[edge].append(trigger)
+        else:
+            self.events_mesh[edge] = [trigger]
+
+    def orient(self):
+        for trigger in self.triggers:
+            self.orient_trigger(self, trigger)
+
+    def transition(self, state):
+        #mildy sure taking a 2^n - 2^k where n and k are nonzero are unique
+        edge = self.find_edge(self.current,state)
+        self.current = state
+
+        for trigger in self.trigger_mesh[edge]:
+            trigger.trigger() #suck it
+
+    def find_edge(self, state1, state2):
+        edge = self.states[state1] - self.states[state2]
+        return edge
+
+    def add_trigger(self, trigger):
+        self.orient_trigger(self, trigger)
+
+        self.trigers.append(trigger)
+
+    def remove_trigger(self, trigger):
+        edge = self.find_edge(trigger.state1, trigger.state2)
+
+        for trigger in self.triggers_mesh[edge]:
+            if trigger.key == trigger:
+                triggers_mesh[edge].remove(trigger)
+
+        self.triggers.remove(trigger)
+
