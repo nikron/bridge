bea3a68bc3cbbf1da3f7e6c067cfa41fa9c38031
Nikhil Bysani <nbysani2@illinois.edu>
2013-02-25 08:47:10 -0600

    Added some more commands and created a variable command method

--- bridge/services/insteon/insteon_im_protocol/command/command.py
+++ bridge/services/insteon/insteon_im_protocol/command/command.py
@@ -36,5 +36,27 @@ def _create_direct_static_standard_command(name, cmd1, cmd2):
 
     return type(name, (InsteonCommand,), {'__init__' : __init__})
 
+def _create_direct_variable_standard_command(name, cmd1):
+    def __init__(self, address, cmd2):
+        InsteonCommand.__init__(self, address, False, False, False, False, 3, cmd1, cmd2)
+
+    return type(name, (InsteonCommand,), {'__init__' : __init__})
+
+
+#The long wall of standard insteon commands, names should be self explinatory
+AssignToAllLinkGroup =  _create_direct_variable_standard_command('AssignToAllLinkGroup', b'\x01')
+DeleteFromAllLinkGroup =  _create_direct_variable_standard_command('DeleteFromAllLinkGroup', b'\x02')
+ProductDataRequest = _create_direct_static_standard_command('ProductDataRequest', b'\x03', b'\x00') 
+FXUsernameRequest = _create_direct_static_standard_command('FXUsernameRequest', b'\x03', b'\x01') 
+DeviceTextStringRequest = _create_direct_static_standard_command('DeviceTextStringRequest', b'\x03', b'\x02') 
+
+EnterLinkingMode =  _create_direct_variable_standard_command('EnterLinkingMode', b'\x09')
+EnterUnlinkingMode =  _create_direct_variable_standard_command('EnterUnlinkingMode', b'\x0A')
+
+GetINSTEONVersion = _create_direct_static_standard_command('GetINSTEONVersion', b'\x0D', b'\x00') 
+
+Ping = _create_direct_static_standard_command('Ping', b'\x0F', b'\x00') 
+IDRequest = _create_direct_static_standard_command('IDRequest', b'\x10', b'\x00') 
 TurnOn = _create_direct_static_standard_command('TurnOn', b'\x11', b'\x00') 
+TurnOnFast = _create_direct_static_standard_command('TurnOnFast', b'\x12', b'\x00') 
 TurnOff = _create_direct_static_standard_command('TurnOff', b'\x13', b'\x00') 
