f68b2ba186159198a32dc2a0b231d822ac55fcae
Nikhil Bysani <nbysani2@illinois.edu>
2013-02-26 11:54:02 -0600

    moved protocol logic from the im to insteon protocol

--- bridge/services/im/im_service.py
+++ bridge/services/im/im_service.py
@@ -21,31 +21,11 @@ class InsteonIMService(BridgeService):
                 self.handle_im_communication()
 
     def handle_im_communication(self):
-        rsp = self.im_ser.read(1)
+        buf = inseon_im_protocol.read_command(self.im_ser)
 
-        if rsp == b'\x02':
-            
-            im_cmd = self.im_ser.read(1)
-            if b == b'\x62':
-                control = self.im_ser.read(4)
-                bs = bitstring.BitString(c[3])
-
-                if bs[7] == True:
-                    left = self.im_ser.read(16)    
-                    buf = im_cmd + control + left
-                else:
-                    left = self.im_ser.read(2)    
-                    buf = im_cmd + control + left
-            else: 
-                to_read = insteon_im_protocol.get_response_length(b)
-                buf = self.im_ser.read(to_read)
-                buf = im_cmd + buf
-
+        if buf is not None:
             update = insteon_im_protocol.decode(buf)
             self.update_model(update) #{'id' : b'\asdfsadf\', 'status' : 100 }
-
-        else:
-            logging.error("Didn't get a start of text for first byte, communications messed up.")
 
     def update_model(self, update):
         self.remote_service_method('model', 'update', update)
--- insteon_protocol/insteon_im_protocol.py
+++ insteon_protocol/insteon_im_protocol.py
@@ -1,3 +1,6 @@
+import logging
+import bitstring
+
 #you can only know how many bytes to read after you read a byte
 #from the device, so here is a dict that tells you how much you
 # need to read
@@ -15,7 +18,7 @@ modemCommands = {
 
                     b'\x60': 7, # Get IM Info
                     b'\x61': 4, # Send All Link Command
-                    b'\x62': 7, # Send Standard or Extended Message
+                    b'\x62': 6, # Send Standard or Extended Message
                     b'\x63': 3, # Send X10
                     b'\x64': 3, # Start All Linking
                     b'\x65': 1, # Cancel All Linking
@@ -36,8 +39,34 @@ modemCommands = {
                     b'\x73': 4, # Get IM Configuration
                     }
 
-def get_response_length(byte):
-    return modemCommands[byte]
+
+#function to read the command bytes from an im
+#im has to support .read(num)
+def read_command(im):
+    buf = None
+
+    rsp = self.im_ser.read(1)
+    if rsp == b'\x02':
+            
+        im_cmd = self.im_ser.read(1)
+        if im_cmd == b'\x62': #62 can be either 6 or 20 bytes left to read
+            control = self.im_ser.read(4) #control bytes of insteon messages
+            bs = bitstring.BitString(control[3])
+
+            if bs[7] == True:
+                left = self.im_ser.read(16) #16 bytes left for ed insteon
+                buf = im_cmd + control + left
+            else:
+                left = self.im_ser.read(2) #two bytes left for a sd insteon    
+                buf = im_cmd + control + left
+            else: 
+                to_read = modemCommands[b]
+                buf = self.im_ser.read(to_read)
+                buf = im_cmd + buf
+    else:
+        logging.error("Didn't get a start of text for first byte, communications messed up.")
+
+    return buf
 
 def decode(buf):
     
