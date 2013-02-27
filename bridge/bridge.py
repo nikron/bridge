3008bb59f20da1f7934ae4147e63dff35752c249
Nikhil Bysani <nbysani2@illinois.edu>
2013-02-27 12:13:02 -0600

    added the start of parsing opts to bridge

--- bridge/bridge.py
+++ bridge/bridge.py
@@ -1,10 +1,24 @@
 import sys
 import os.path
+current_dir = os.path.dirname(__file__)
 #hack to add top directory to import searching path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+sys.path.append(os.path.abspath(os.path.join(current_dir, os.path.pardir)))
+
+import argparse
 
 import bridgehub
+from bridgeconfig import BridgeConfiguration
 from services.io.insteon_service import InsteonIMService
+
+def parse_opts(default):
+    parser = argparse.ArgumentParser(description='Control your devices!')
+    parser.add_argument('-c', '--configuration', help='File to read configuration from', default=default)
+    parser.add_argument('-d', '--directory', help='Directory to store data') 
+
+    args = parser.parse_args()
+    config = BridgeConfiguration(args.configuration)
+
+    return config
 
 #have to dynamcially read this from a file and rutime options in future
 def configuration():
@@ -18,6 +32,10 @@ def configuration():
 def main():
     #TODO: reading in options
     #TODO: reading in configuration
+    default_config_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'bridge.config')
+
+    config = parse_opts(default_config_file)
+
     config = configuration()
 
     hub = bridgehub.BridgeHub(config)
---
+++ bridge/bridgeconfig.py
@@ -0,0 +1,6 @@
+class BridgeConfiguration():
+    def __init__(self, conf_file):
+        self.file = conf_file
+
+    def load_from_file():
+        pass
