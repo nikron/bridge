a7dc2bb8025aa25d1828d1352358367dc6b17012
Nikhil Bysani <nbysani2@illinois.edu>
2013-03-05 15:49:17 -0600

    Cleaned up a little

--- bridge/services/io/insteon/__init__.py
+++ bridge/services/io/insteon/__init__.py
@@ -1,10 +1,3 @@
-from bridge.services.io.service import IOService
-from insteon_protocol import command, insteon_im_protocol, device
+__all__ = ["idiom", "InsteonIMService"]
 
-class InsteonIMService(IOService):
-    def handle_read_interface(self):
-        buf = inseon_im_protocol.read_command(self.im_ser)
-
-        if buf is not None:
-            update = insteon_im_protocol.decode(buf)
-            self.update_model(update) #{'id' : b'\asdfsadf\', 'status' : 100 }
+from bridge.services.io.insteon.service import InsteonIMService
---
+++ bridge/services/io/insteon/service.py
@@ -0,0 +1,10 @@
+from bridge.services.io.service import IOService
+from insteon_protocol import command, insteon_im_protocol, device
+
+class InsteonIMService(IOService):
+    def handle_read_interface(self):
+        buf = inseon_im_protocol.read_command(self.im_ser)
+
+        if buf is not None:
+            update = insteon_im_protocol.decode(buf)
+            self.update_model(update) #{'id' : b'\asdfsadf\', 'status' : 100 }
