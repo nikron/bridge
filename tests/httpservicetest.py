91c24fb78bb9f189baeef4c8226e02ba3782934b
Nikhil Bysani <nbysani2@illinois.edu>
2013-03-30 23:26:04 -0500

    httpservice test

---
+++ tests/httpservicetest.py
@@ -0,0 +1,18 @@
+import unittest
+from multiprocessing import Pipe
+from bridge.services.net.http_service import HTTPAPIService
+from bottle import HTTPError
+
+class TestHTTPService(unittest.TestCase):
+    def setUp(self):
+        (ours, its) = Pipe()
+
+        self.serv = HTTPAPIService(its)
+        self.ours = ours
+
+    def test_info(self):
+        api = self.serv.bridge_information()()
+        self.assertIsNotNone(api)
+
+    def test_valid_uuid(self):
+        self.assertRaises(HTTPError, self.serv.check_valid_uuid, 'afadf')
