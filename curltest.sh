dff5c2e84e4720feca6e321875d9d5ee20b05329
Nikhil Bysani <nbysani2@illinois.edu>
2013-03-31 15:59:28 -0500

    "integration test"

---
+++ curltest.sh
@@ -0,0 +1,2 @@
+curl -v -H "Content-Type: application/json; charset=utf-8" -X POST -d '{"name" : "Test", "service" : "insteon", "asset class" : "ApplianceLinc V2", "real id" : "1ad98e"}' localhost:8080/assets
+curl localhost:8080/assets
