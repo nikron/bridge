595b7e89a873dd96104bf3a87643fb3e498e513f
Nikhil Bysani <nbysani2@illinois.edu>
2013-03-30 19:56:12 -0500

    Have coverage actually check over the code.

--- run_tests.py
+++ run_tests.py
@@ -7,7 +7,7 @@ def main():
     this_dir = os.path.dirname(__file__)
     test_dir = os.path.join(os.path.abspath(this_dir), 'tests')
 
-    cov = coverage.coverage()
+    cov = coverage.coverage(source=['bridge', 'insteon_protocol'])
     cov.start()
     suite = unittest.defaultTestLoader.discover(test_dir, pattern="*test.py")
     unittest.TextTestRunner(verbosity=2).run(suite)
