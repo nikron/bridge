95948df7e8b1f29a75080ea1a93886fe47cf12a4
Nikhil Bysani <nbysani2@illinois.edu>
2013-04-02 20:20:01 -0500

    made it so you could run tests without installing coverage

--- run_tests.py
+++ run_tests.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 import os.path
 import unittest
-import coverage
+try:
+    import coverage
+except ImportError:
+    coverage = None
 
 def main():
     this_dir = os.path.dirname(__file__)
     test_dir = os.path.join(os.path.abspath(this_dir), 'tests')
 
-    cov = coverage.coverage(source=['bridge', 'insteon_protocol'])
-    cov.start()
+    if coverage:
+        cov = coverage.coverage(source=['bridge', 'insteon_protocol'])
+        cov.start()
+
     suite = unittest.defaultTestLoader.discover(test_dir, pattern="*test.py")
     unittest.TextTestRunner(verbosity=2).run(suite)
-    cov.stop()
-    cov.save()
-    cov.report()
+
+    if coverage:
+        cov.stop()
+        cov.save()
+        cov.report()
 
 
 if __name__ == "__main__":
