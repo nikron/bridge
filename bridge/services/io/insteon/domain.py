2494784d5489332a5b8dcfdd8d1b9b3cdb7d030b
Andrew Mintner <mintner1@illinois.edu>
2013-04-21 18:14:10 -0500

    Further tightened up domain.py

--- bridge/services/io/insteon/domain.py
+++ bridge/services/io/insteon/domain.py
@@ -1,8 +1,65 @@
+import abc
+import gevent
+import gevent.event
 import gevent.select
 import serial
-from bridge.services.io.devcore import Domain
+from bridge.services.io.devcore import Domain, Locator
 from bridge.services.io.profiles import *
 from insteon_protocol import insteon_im_protocol
+
+class _InsteonDevice(Device):
+    def __init__(self, locator, profile):
+        super().__init__(locator, profile)
+    
+    def control_async(self, attribute, value):
+        # Validate arguments
+        assert attribute != None
+        assert value != None
+        if not attribute in self.profile.attributes:
+            raise ValueError("Only attributes of this device are permitted")
+        if not attribute.space.validate(value):
+            raise ValueError("Illegal attribute value")
+            
+        # Delegate to the profile
+        res = AsyncResult()
+        fn = self.profile._control
+        gevent.spawn(fn, self.locator, attribute, value).link(res)
+        return res
+    
+    def interrogate_async(self, attribute):
+        # Validate arguments
+        assert attribute != None
+        if not attribute in self.profile.attributes:
+            raise ValueError("Only attributes of this device are permitted")
+        if not attribute.space.validate(value):
+            raise ValueError("Illegal attribute value")
+            
+        # Delegate to the profile
+        res = AsyncResult()
+        fn = self.profile._interrogate
+        gevent.spawn(fn, self.locator, attribute).link(res)
+        return res
+
+class InsteonDeviceProfile(DeviceProfile, metaclass=abc.ABCMeta):
+    def bind(self, locator):
+        # Validate arguments
+        if locator == None or not isinstance(locator, Locator):
+            raise ValueError("The specified value is not a locator")
+        if not isinstance(locator.domain, InsteonDomain):
+            raise ValueError("This profile supports only Insteon devices")
+        if not isinstance(locator.address, bytes) or len(locator.address) != 3:
+            raise ValueError("The specified locator address is not valid")
+            
+        # Delegate to the InsteonDomain
+        return locator.domain._bind(locator, self)
+    
+    @abc.abstractmethod
+    def _control(self, locator, attribute, value):
+        pass
+        
+    @abc.abstractmethod
+    def _interrogate(self, locator, attribute):
+        pass
 
 class InsteonDomain(Domain):
     _plist = [
@@ -11,11 +68,19 @@ class InsteonDomain(Domain):
     ]
     _pmap = {p.identifier: p for p in _plist}
     
-    def __init__(self, modem_device):
+    def __init__(self, identifier, modem_device):
+        super().__init__(identifier)
+        self._bindings = {}
         self._serdev = serial.Serial(modem_device, 19200)
     
-    def find_profile(self, identifier):
-        return self._pmap.get(identifier)
+    def _bind(self, locator, profile):
+        # Store the binding, unless one already exists
+        if address in self._bindings:
+            raise ValueError("The specified address is already bound")
+        self._bindings[address] = profile
+        
+        # Produce an InsteonDevice for the caller
+        return _InsteonDevice(locator, profile)
     
     def monitor(self):
         while True:
@@ -29,5 +94,5 @@ class InsteonDomain(Domain):
     def profiles(self):
         return self._plist
 
-    def send_cmd(self, address, cmd):
+    def _xmit(self, address, cmd):
         raise NotImplementedError()
