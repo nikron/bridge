62a7151d52d271d02a6442b73f3738e4b32d1ddf
Nikhil Bysani <nbysani2@illinois.edu>
2013-04-24 21:07:21 -0500

    Got rid of commented code

--- bridge/services/io/insteon/__init__.py
+++ bridge/services/io/insteon/__init__.py
@@ -1,7 +1,3 @@
 """
 Insteon io service and its idiom.
 """
-#from bridge.services.io.insteon.service import InsteonIMService
-#from bridge.services.io.insteon import idiom
-
-#__all__ = ["idiom", "InsteonIMService"]
