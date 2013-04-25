b9ef70ca81dc5d4875253d5ceebfb3ccaca46611
Nikhil Bysani <nbysani2@illinois.edu>
2013-04-24 21:02:11 -0500

    Update base class for IO services.

--- bridge/services/io/service.py
+++ bridge/services/io/service.py
@@ -8,8 +8,17 @@ from bridge.service import BridgeService
 
 class IOService(BridgeService):
     """
-    An abstraction of a BridgeService, meant to run IO
-    on a paticular interface.
+    An abstraction of an IO BridgeService, meant to run IO
+    on a paticular interface module/device.
+
+    :param name: Name of the IO service.
+    :type name: str
+
+    :param name: Path of the file used for I/O.
+    :type file_name: str
+
+    :param hub_connection: Connection to BridgeHub.
+    :type hub_connection: Pipe
     """
 
     def __init__(self, name, file_name, hub_connection):
@@ -18,6 +27,22 @@ class IOService(BridgeService):
 
         self.read_list = [self.hub_connection]
         self.pending = []
+
+    def asset_info(self, real_id):
+        """
+        Get more info about device (by querying the IO).
+        Must be implemented by each IO service.
+
+        :param real_id: Real ID to get asset information about.
+        :type real_id: object
+        """
+        raise NotImplementedError
+
+    def read_io(self):
+        """
+        Read the file, each IO service must implement this.
+        """
+        raise NotImplementedError
 
     def run(self):
         self.mask_signals()
@@ -39,30 +64,46 @@ class IOService(BridgeService):
                 self.read_io()
 
     def update_model(self, real_id, update):
-        """Send an upate to model, it will be decoded by an idiom."""
+        """
+        Send an upate to model, it will changed to a asset transition(s) by an idiom.
+
+        :param real_id: ID used by the the I/O.
+        :type real_id: object
+
+        :param update: Update to send to model.
+        :type update: object
+        """
         logging.debug("Updating model with {0}".format(update))
         self.remote_async_service_method('model', 'io_update', self.name, real_id, update)
 
-    def _create_fd(self, file_name):
-        raise NotImplementedError
-
     def write_io(self, buf):
+        """
+        Write a buffer to the file descriptor.
+
+        :param buf: Buffer to write.
+        :type buf: bytes
+        """
         logging.debug("Writing command {0}.".format(str(buf)))
         if self.io_fd:
             self.io_fd.write(buf)
         else:
             self.pending.append(buf)
 
-    def read_io(self):
-        """Read the interface, each IO service must implmeenet this."""
-        raise NotImplementedError
-
-    def asset_info(self, real_id):
-        """Get more info about device."""
-        raise NotImplementedError
-
     def notify_not_connected(self):
+        """
+        Notify the model that the this service isn't connected.
+        """
         self.remote_async_service_method('model', 'io_service_offline', self.name)
 
     def notify_connected(self):
+        """
+        Notify the model that the this service is connected.
+        """
         self.remote_async_service_method('model', 'io_service_online', self.name)
+
+    def _create_fd(self, file_name):
+        """
+        Create the file descriptor from the file name,
+        each IO service should implement this.
+        """
+        raise NotImplementedError
