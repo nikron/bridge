b5211e92b69d370dbe11a8578d46e94783614264
Nikhil Bysani <nbysani2@illinois.edu>
2013-04-24 20:15:17 -0500

    Base module documentation

--- bridge/__init__.py
+++ bridge/__init__.py
@@ -1,5 +1,5 @@
 """
-Program that controls home automation hardware devices
-by modeling them.  Accepts commands to change model,
-and then tries to change actual devices.
+Program that controls home automation hardware devices by modeling them.
+Uses a multiprocess model, with an individual process for each event task.
+Accepts commands to change model, and then tries to change actual devices.
 """
