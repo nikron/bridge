8176d77ab1fa3eed77ce6b59543a952a7a5500e3
Nikhil Bysani <nbysani2@illinois.edu>
2013-04-24 21:06:39 -0500

    Documentation for types

--- bridge/services/io/types.py
+++ bridge/services/io/types.py
@@ -6,8 +6,16 @@ from bridge.services.io.insteon.idiom import InsteonIdiom
 
 class IOConfig():
     """
-    The model recieves one of these for every running IO service, so that
-    it knows how to communicate with it.
+    Find and init an IO service and idiom associated with a protocl/ IO type.
+
+    :param name: IO service name.
+    :type name: str
+
+    :param protocol: Protocol to find service/idiom combo.
+    :type protocol: str
+
+    :param file_name: Path the IO service should control.
+    :type file_name: str
     """
 
     io_types = {
@@ -23,13 +31,16 @@ class IOConfig():
         self.idiom = self.io_types[self.io_type][1]
 
     def create_service(self, hub_con):
-        """Return initialized service."""
+        """
+        Return initialized service.
 
+        :param hub_con: Hub connection to init io service with.
+        :type hub_con: Pipe
+        """
         return self.service(self.name, self.file_name, hub_con)
 
     def model_idiom(self):
         """
         Create the idiom that the model needs to communicate with the service.
         """
-
         return self.idiom(self.name)
