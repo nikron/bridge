e975a8f395c8d0d5ccccc6672cc657bcf9c12778
Nikhil Bysani <nbysani2@illinois.edu>
2013-04-24 20:20:30 -0500

    logging queue documentation

--- bridge/logging_service.py
+++ bridge/logging_service.py
@@ -1,7 +1,6 @@
 """
-Wrapper around python API for a logging thread
-that listens on a queue, our queue is implemented by
-multiprocess Queue.
+Wrapper around python API for a logging thread that listens on a queue;
+the queue we use is the multiprocessing Queue for obvious reasons.
 """
 
 import logging
@@ -11,29 +10,40 @@ import sys
 
 class LoggingService():
     """
-    Creates a thred that listens for LogRecords and logs them
-    to log_file or stderr.
+    Intializes a thread that listens on a queue to log messages to a file or stderr.
+
+    :param log_file: Path to the file we should log to.
+    :type log_file: str
     """
+    LOG_FORMAT_BRIEF = "%(levelname)s :: %(msg)s"
+
     def __init__(self, log_file, stderr):
         self.queue = Queue()
 
-        if stderr:  
+        if stderr:
             handler = logging.StreamHandler(sys.stderr)
         else:
             handler = logging.FileHandler(log_file)
 
-        #formatter = logging.Formatter('%(asctime)s: %(pathname)s - %(levelname)s :: %(msg)s')
-        formatter = logging.Formatter('%(levelname)s :: %(msg)s')
+        formatter = logging.Formatter(self.LOG_FORMAT_BRIEF)
         handler.setFormatter(formatter)
-
         self._queuelistener = logging.handlers.QueueListener(self.queue, handler)
 
     def start(self):
-        """Start the logging service thread."""
+        """
+        Start the logging service thread.
+        """
         self._queuelistener.start()
 
 def service_configure_logging(queue):
-    """Configure the root logger to send log records to a queue."""
+    """
+    Configure the current root logger to send log records to a queue.
+    Note the root logger stays the same after you fork, this should be used only once.
+    Otherwise logs will get repeated.
+
+    :param queue: The queue to send logs to.
+    :type queue: Queue
+    """
     handler = logging.handlers.QueueHandler(queue)
     root = logging.getLogger()
     root.addHandler(handler)
