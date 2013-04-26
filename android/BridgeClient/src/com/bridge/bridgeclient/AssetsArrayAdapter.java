05650aed46258da87ab4822792fe24ccc373a263
Nikhil Bysani <nbysani2@illinois.edu>
2013-04-25 20:46:30 -0500

    moved the control of the asset views into adapter
    no idea why I thought the fragment doing an adapter's job
    was correct.  I'm bad at this

--- android/BridgeClient/src/com/bridge/bridgeclient/AssetsArrayAdapter.java
+++ android/BridgeClient/src/com/bridge/bridgeclient/AssetsArrayAdapter.java
@@ -1,36 +1,47 @@
 package com.bridge.bridgeclient;
 
+import android.content.Intent;
 import android.content.Context;
 
-import android.widget.ArrayAdapter;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.os.Bundle;
+import android.os.Handler;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
 import org.jraf.android.backport.switchwidget.Switch;
 //import android.widget.Switch;
 
-import android.widget.CompoundButton;
+import org.json.JSONException;
 
-public class AssetsArrayAdapter extends ArrayAdapter<Asset>
+public class AssetsArrayAdapter extends ArrayAdapter<Asset> implements BridgeClientReceiver.Receiver
 {
-    final Context context;
-    final AssetsFragment fragment;
+    final SherlockFragmentActivity context;
+    BridgeClientReceiver receiver;
+
     static final int binaryAsset = R.xml.asset_binary;
     static final int rangeAsset = R.xml.asset_range;
     static final int unknownAsset = R.xml.asset_unknown;
     static final int textViewResourceId = R.id.assetname;
     static final int controlResourceId = R.id.assetcontrol;
 
-    public AssetsArrayAdapter(Context context, AssetsFragment fragment)
+
+    public AssetsArrayAdapter(SherlockFragmentActivity context)
     {
         super(context, binaryAsset, textViewResourceId);
         this.context = context;
-        this.fragment = fragment;
+
+        receiver = new BridgeClientReceiver(new Handler());
+        receiver.setReceiver(this);
     }
 
     @Override
@@ -49,7 +60,7 @@ public class AssetsArrayAdapter extends ArrayAdapter<Asset>
                 mainSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                         String message = asset.setCurrentMainState(isChecked);
-                        fragment.sendAssetPatch(asset.getURL(), message);
+                        sendAssetPatch(asset.getURL(), message);
                 }});
 
                 break;
@@ -63,5 +74,79 @@ public class AssetsArrayAdapter extends ArrayAdapter<Asset>
         assetName.setText(asset.toString());
 
         return layout;
+    }
+
+    @Override
+    public void onReceiveResult(int resultCode, Bundle resultData)
+    {
+        switch (resultCode)
+        {
+            case BridgeClientService.STATUS_RUNNING:
+                context.setSupportProgressBarVisibility(true);
+                break;
+
+            case BridgeClientService.STATUS_PROGRESS:
+                context.setSupportProgress(resultData.getInt(BridgeClientService.PROGRESS_KEY));
+                break;
+
+            case BridgeClientService.STATUS_GET_ASSETS_FINISHED:
+                context.setSupportProgressBarVisibility(false);
+                newAssetData(resultData.getStringArray(BridgeClientService.RESULTS_KEY));
+                break;
+
+            case BridgeClientService.STATUS_ERROR:
+                context.setSupportProgressBarVisibility(false);
+                displayError(resultData.getString(Intent.EXTRA_TEXT));
+                clear();
+                break;
+
+            default:
+                context.setSupportProgressBarVisibility(false);
+                break;
+        }
+    }
+
+    public void refresh()
+    {
+        final Intent intent = new Intent(Intent.ACTION_SYNC, null, context, BridgeClientService.class);
+        intent.putExtra(BridgeClientService.RECEIVER_KEY, receiver);
+        intent.putExtra(BridgeClientService.COMMAND_KEY, BridgeClientService.GET_ASSETS_COMMAND);
+
+        context.startService(intent);
+    }
+
+    private void newAssetData(String[] assetsJSON)
+    {
+        clear();
+        for (int i = 0; i < assetsJSON.length; i++)
+        {
+            try
+            {
+                add(new Asset(assetsJSON[i]));
+            }
+            catch (JSONException e)
+            {
+                displayError(e.getMessage());
+            }
+        }
+
+        notifyDataSetChanged();
+    }
+
+    private void sendAssetPatch(String url, String patch)
+    {
+        final Intent intent = new Intent(Intent.ACTION_SYNC, null, context, BridgeClientService.class);
+        intent.putExtra(BridgeClientService.RECEIVER_KEY, receiver);
+        intent.putExtra(BridgeClientService.COMMAND_KEY, BridgeClientService.PATCH_ASSET_COMMAND);
+        intent.putExtra(BridgeClientService.URL_KEY, url);
+        intent.putExtra(BridgeClientService.PATCH_KEY, patch);
+
+        context.startService(intent);
+    }
+
+    private void displayError(String message)
+    {
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
--- android/BridgeClient/src/com/bridge/bridgeclient/AssetsFragment.java
+++ android/BridgeClient/src/com/bridge/bridgeclient/AssetsFragment.java
@@ -1,28 +1,21 @@
 package com.bridge.bridgeclient;
 
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.widget.ListView;
+
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.app.SherlockListFragment;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.MenuInflater;
 
-import android.os.Bundle;
-import android.os.Handler;
-
-import android.content.Intent;
-
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.LayoutInflater;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import org.json.JSONException;
-
-public class AssetsFragment extends SherlockListFragment implements BridgeClientReceiver.Receiver
+public class AssetsFragment extends SherlockListFragment
 {
     SherlockFragmentActivity context;
-    BridgeClientReceiver receiver;
     AssetsArrayAdapter assetsAdapter;
     ListView devices;
 
@@ -38,11 +31,9 @@ public class AssetsFragment extends SherlockListFragment implements BridgeClient
         setHasOptionsMenu(true);
 
         context = getSherlockActivity();
-        assetsAdapter = new AssetsArrayAdapter(context, this);
+        assetsAdapter = new AssetsArrayAdapter(context);
         setListAdapter(assetsAdapter);
 
-        receiver = new BridgeClientReceiver(new Handler());
-        receiver.setReceiver(this);
     }
 
     @Override
@@ -65,85 +56,11 @@ public class AssetsFragment extends SherlockListFragment implements BridgeClient
         switch (item.getItemId())
         {
             case R.id.menu_refresh_assets:
-                refresh();
+                assetsAdapter.refresh();
                 return true;
 
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    @Override
-    public void onReceiveResult(int resultCode, Bundle resultData)
-    {
-        switch (resultCode)
-        {
-            case BridgeClientService.STATUS_RUNNING:
-                context.setSupportProgressBarVisibility(true);
-                break;
-
-            case BridgeClientService.STATUS_PROGRESS:
-                context.setSupportProgress(resultData.getInt(BridgeClientService.PROGRESS_KEY));
-                break;
-
-            case BridgeClientService.STATUS_GET_ASSETS_FINISHED:
-                context.setSupportProgressBarVisibility(false);
-                newAssetData(resultData.getStringArray(BridgeClientService.RESULTS_KEY));
-                break;
-
-            case BridgeClientService.STATUS_ERROR:
-                context.setSupportProgressBarVisibility(false);
-                displayError(resultData.getString(Intent.EXTRA_TEXT));
-                assetsAdapter.clear();
-                break;
-
-            default:
-                context.setSupportProgressBarVisibility(false);
-                break;
-        }
-    }
-
-    private void displayError(String message)
-    {
-        Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    private void newAssetData(String[] assetsJSON)
-    {
-        assetsAdapter.clear();
-        for (int i = 0; i < assetsJSON.length; i++)
-        {
-            try
-            {
-                assetsAdapter.add(new Asset(assetsJSON[i]));
-            }
-                catch (JSONException e)
-            {
-                displayError(e.getMessage());
-            }
-        }
-
-        assetsAdapter.notifyDataSetChanged();
-    }
-
-    public void sendAssetPatch(String url, String patch)
-    {
-        final Intent intent = new Intent(Intent.ACTION_SYNC, null, context, BridgeClientService.class);
-        intent.putExtra(BridgeClientService.RECEIVER_KEY, receiver);
-        intent.putExtra(BridgeClientService.COMMAND_KEY, BridgeClientService.PATCH_ASSET_COMMAND);
-        intent.putExtra(BridgeClientService.URL_KEY, url);
-        intent.putExtra(BridgeClientService.PATCH_KEY, patch);
-
-        context.startService(intent);
-    }
-
-    private void refresh()
-    {
-        final Intent intent = new Intent(Intent.ACTION_SYNC, null, context, BridgeClientService.class);
-        intent.putExtra(BridgeClientService.RECEIVER_KEY, receiver);
-        intent.putExtra(BridgeClientService.COMMAND_KEY, BridgeClientService.GET_ASSETS_COMMAND);
-
-        context.startService(intent);
     }
 }
