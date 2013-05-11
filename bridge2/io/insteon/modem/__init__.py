17cf71d7b24deb408ac4cc64a53188353bf745b4
Nikhil Bysani <nbysani2@illinois.edu>
2013-05-11 17:59:51 -0500

    Removing mintener's stuff so I have full ownership
    so i can set MIT license

--- bridge2/io/insteon/modem/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-from .client import InsteonClient
-from .messages import ExtInsteonMessage, InsteonMessage
-
-__all__ = [
-    b"InsteonClient",
-    b"ExtInsteonMessage",
-    b"InsteonMessage"
-]
--- bridge2/io/insteon/profiles/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-from .power import PowerDeviceProfile
-from .dimmablepower import DimmablePowerDeviceProfile
-
-__all__ = [
-    b"PowerDeviceProfile",
-    b"DimmablePowerDeviceProfile"
-]
