814fbf8a09dac5f2febc5af5b067f6e69757d0df
Nikhil Bysani <nikron@gmail.com>
2013-06-11 09:29:30 -0500

    made upb mini do something useful

--- upb_mini.py
+++ upb_mini.py
@@ -5,7 +5,7 @@ from upb.pim import write_message
 import upb
 
 ser = serial.Serial('/dev/ttyUSB0', '4800')
-msg = upb.UPBGoToLevel(0)
-msg.destination_id = 5
+msg = upb.UPBGoToLevel(100)
+msg.destination_id = 45
 
 write_message(ser, msg)
